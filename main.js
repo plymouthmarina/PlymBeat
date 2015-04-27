2bb993dc7a6425f4d5aca827df6a4f9f413d58fd
Gerben Neven <gerbyzation@me.com>
2015-04-27 11:20:49 +0100

    seperates JS from index.html

---
+++ main.js
@@ -0,0 +1,41 @@
+var pulseInterval;
+
+$(document).ready(function(e) {
+  
+  pulseInterval = setInterval(pulseAnimation,100);
+      
+  });
+
+var frame = 1;
+  
+function pulseAnimation(){
+   
+  var top = 269 * frame;
+     
+  $('#pulseAnimation').css('backgroundPosition', '0px ' + '-'+ top + 'px');
+  
+  frame = ++frame % 19;
+
+  if (frame < 19){
+     frame++;
+  }
+  else
+  {
+     frame = 1; 
+  }
+} 
+
+/*
+function pulseAnimation(){
+   
+  var left = 128 * frame;
+     
+$('#pulseAnimation').css('backgroundPosition','-'+left+'px 0px');
+ 
+  if (frame < 9){
+   frame++;
+}
+else
+{
+   frame = 1; 
+} */
